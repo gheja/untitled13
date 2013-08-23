e509db17278df57c0f826dc52a0119d12f0e43fb
Gabor Heja <gheja@cubeone>
2013-08-23 19:00:40 +0000

    Field is now 20x20 tiles, it fits in 1280x720.

--- src/13312.js
+++ src/13312.js
@@ -134,9 +134,9 @@ window.onload = function()
 	
 	A.render_layer_map = function()
 	{
-		for (j=0; j<32; j++)
+		for (j=0; j<20; j++)
 		{
-			for (i=0; i<32; i++)
+			for (i=0; i<20; i++)
 			{
 				A.texture_show(0, 0, 1280 / 2 - 64 / 2 + i * 32 - j * 32, i * 16 + j * 16);
 			}
