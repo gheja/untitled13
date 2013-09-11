e7b4164f7b61044c5df01c7c583575327ac08231
Gabor Heja <gheja@cubeone>
2013-09-11 20:24:03 +0000

    build.sh: updated build script to create "client", "server" and "all" archives.

--- build.sh
+++ build.sh
@@ -13,7 +13,9 @@ try()
 
 [ -d build ] || try mkdir -p -m 700 build
 [ -d build/compiler ] || try mkdir -p -m 700 build/compiler
-[ -d build/js13kgames_entry ] || try mkdir -p -m 700 build/js13kgames_entry
+[ -d build/client ] || try mkdir -p -m 700 build/client
+[ -d build/server ] || try mkdir -p -m 700 build/server
+[ -d build/all ] || try mkdir -p -m 700 build/all
 
 which java 2>/dev/null >/dev/null
 if [ $? != 0 ]; then
@@ -44,45 +46,71 @@ if [ ! -e build/compiler/compiler.jar ]; then
 fi
 echo "* Closure Compiler seems to be good."
 
-echo "* Cleaning up build directory..."
-rm build/js13kgames_entry/* || /bin/true
+echo "* Cleaning up build directories..."
+rm build/client/* || /bin/true
+rm build/server/* || /bin/true
+rm build/all/* || /bin/true
 
-echo "* Copying resources..."
-try cp -xarv src/* build/js13kgames_entry/
+echo "* Client"
 
-echo "* Removing debug parts and renaming some variables..."
-cat build/js13kgames_entry/13312.js | sed \
+echo "  * Optimizing index.html..."
+cat src/index.html | tr '\n' ' ' | sed -r 's/\s+/ /g' | sed -e 's/> </></g' > build/client/index.html
+
+echo "  * Removing debug parts and renaming some variables..."
+cat src/13312.js | sed \
 	-e '/DEBUG BEGIN/,/\DEBUG END/{d}' \
 	-e 's/.*[A-Z].log.*//g' \
 	-e 's/\.direction/.d/g' \
 	-e 's/\.position/.p/g' \
-	-e 's/\.speed/.s/g' > build/js13kgames_entry/13312.js.1
-mv build/js13kgames_entry/13312.js.1 build/js13kgames_entry/13312.js
+	-e 's/\.speed/.s/g' > build/client/13312.js
 
-echo "* Running Closure Compiler..."
+echo "  * Running Closure Compiler..."
 try java -jar build/compiler/compiler.jar \
 	--compilation_level ADVANCED_OPTIMIZATIONS \
 	--use_types_for_optimization \
-	--js build/js13kgames_entry/13312.js \
-	--js_output_file build/js13kgames_entry/13312.min.js
+	--js build/client/13312.js \
+	--js_output_file build/client/13312.min.js
 
-try mv -f build/js13kgames_entry/13312.min.js build/js13kgames_entry/13312.js
+try mv -f build/client/13312.min.js build/client/13312.js
 
-echo "* Optimizing index.html..."
-cat build/js13kgames_entry/index.html | tr '\n' ' ' | sed -r 's/\s+/ /g' | sed -e 's/> </></g' > build/js13kgames_entry/index.html.1
-mv build/js13kgames_entry/index.html.1 build/js13kgames_entry/index.html
+echo "* Server"
+
+echo "  * Removing debug parts and renaming some variables..."
+cat src/server.js | sed \
+	-e '/DEBUG BEGIN/,/\DEBUG END/{d}' \
+	-e 's/.*[A-Z].log.*//g' > build/server/server.js
+
+echo "  * Running Closure Compiler..."
+try java -jar build/compiler/compiler.jar \
+	--compilation_level ADVANCED_OPTIMIZATIONS \
+	--use_types_for_optimization \
+	--js build/server/server.js \
+	--js_output_file build/server/server.min.js
+
+try mv -f build/server/server.min.js build/server/server.js
+
+echo "* All"
+
+echo "  * Copying files..."
+cp -xarv build/client/* build/server/* build/all/
 
 try cd build
 
 now=`date +%Y%m%d_%H%M%S`
 git_id=`git log -1 --format="%H"`
-zip_file="13312_${now}_${git_id}.zip"
+zip_file="13312_${now}_${git_id}"
 
-echo "* Creating new archive $zip_file ..."
-try zip $zip_file -r js13kgames_entry
+echo "* Creating new archive ${zip_file}_client.zip ..."
+try zip ${zip_file}_client.zip -r client
+
+echo "* Creating new archive ${zip_file}_server.zip ..."
+try zip ${zip_file}_server.zip -r server
+
+echo "* Creating new archive ${zip_file}_all.zip ..."
+try zip ${zip_file}_all.zip -r all
 
 try cd ..
 
 echo "Done."
 
-du -b ./src ./build/js13kgames_entry ./build/$zip_file
+du -b ./src ./build/client ./build/server ./build/all ./build/${zip_file}_client.zip ./build/${zip_file}_server.zip ./build/${zip_file}_all.zip
