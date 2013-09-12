d41da2381bd750aa08ebfd7a5cfd845beeac4e43
Gabor Heja <gheja@cubeone>
2013-09-12 19:22:31 +0000

    externs.js: updated to contain the server-related stuffs.

--- src/externs.js
+++ src/externs.js
@@ -3,3 +3,10 @@ io.connect = function(a){};
 io.connect.on = function(a, b){};
 io.connect.emit = function(a, b){};
 
+var app = {};
+app.createServer = function(a){};
+
+var io = {};
+io.listen = function(a){};
+io.sockets = {};
+io.sockets.socket = {};
