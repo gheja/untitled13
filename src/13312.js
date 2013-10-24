d7dbbe9d6ca93e44b65fcd066256159adfbe0d55
Gabor Heja <gheja@cubeone>
2013-10-24 23:12:33 +0200

    Removed ammo and reload bars for ObjectPlayer2Crystal objects (fixes #12)

--- src/13312.js
+++ src/13312.js
@@ -1755,16 +1755,24 @@ window.onload = function()
 			}
 			else
 			{
-				A.gfx__render_gui_bar_background(p[0] - 24, p[1] + 6, 48, 14);
-				A.gfx__render_gui_bar(p[0] - 22, p[1] + 8, 44, A.objects[i].health[0] / A.objects[i].health[1] , "#5f0");
-				if (A.objects[i].attack_status == A.ATTACK_STATUS_RELOADING)
+				if (A.objects[i].attack_ammo[1] == 0)
 				{
-					A.gfx__render_gui_bar(p[0] - 22, p[1] + 14, 44, A.objects[i].attack_reload_time[0] / A.objects[i].attack_reload_time[1], "#bbb");
+					A.gfx__render_gui_bar_background(p[0] - 24, p[1] + 6, 48, 8);
+					A.gfx__render_gui_bar(p[0] - 22, p[1] + 8, 44, A.objects[i].health[0] / A.objects[i].health[1], "#5f0");
 				}
 				else
 				{
-					A.gfx__render_gui_bar(p[0] - 22, p[1] + 14, 32, A.objects[i].attack_ammo[0] / A.objects[i].attack_ammo[1], "#ee0");
-					A.gfx__render_gui_bar(p[0] + 10, p[1] + 14, 12, A.objects[i].attack_cycle_time[0] / A.objects[i].attack_cycle_time[1], "#eee");
+					A.gfx__render_gui_bar_background(p[0] - 24, p[1] + 6, 48, 14);
+					A.gfx__render_gui_bar(p[0] - 22, p[1] + 8, 44, A.objects[i].health[0] / A.objects[i].health[1] , "#5f0");
+					if (A.objects[i].attack_status == A.ATTACK_STATUS_RELOADING)
+					{
+						A.gfx__render_gui_bar(p[0] - 22, p[1] + 14, 44, A.objects[i].attack_reload_time[0] / A.objects[i].attack_reload_time[1], "#bbb");
+					}
+					else
+					{
+						A.gfx__render_gui_bar(p[0] - 22, p[1] + 14, 32, A.objects[i].attack_ammo[0] / A.objects[i].attack_ammo[1], "#ee0");
+						A.gfx__render_gui_bar(p[0] + 10, p[1] + 14, 12, A.objects[i].attack_cycle_time[0] / A.objects[i].attack_cycle_time[1], "#eee");
+					}
 				}
 			}
 		}
